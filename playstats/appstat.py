e0b4cb81d082c586f0826949e9aa89c1ebd56e39
Apoorva Pandey <apoorvapandey365@gmail.com>
2017-06-28 22:52:21 +0530

    Fetch total reviews for an app.

--- playstats/appstat.py
+++ playstats/appstat.py
@@ -9,17 +9,25 @@ class AppStat:
         self.content = urlopen(self.app_url).read()
         self.tree = html.fromstring(self.content)
 
-    def get_rating(self):
+    def rating(self):
         selector = CSSSelector('.score')
         match = self.tree.xpath(selector.path)
         rating = float(match[0].text)
         return rating
 
-    def get_all_ratings(self):
+    def all_ratings(self):
         # Returns number of ratings corresponding to different stars
         # in the form of dictionary.
         selector = CSSSelector('.bar-number')
         matches = self.tree.xpath(selector.path)
         ratings = [int(x.text.replace(',', '')) for x in matches]
-        ratings_dict = {star: no_of_ratings for (star, no_of_ratings) in zip(range(5, 0, -1), ratings)}
+        ratings_dict = {star: no_of_ratings for (star, no_of_ratings) in
+                        zip(range(5, 0, -1), ratings)}
         return ratings_dict
+
+    def total_reviews(self):
+        # Returns total number of reviews for an app.
+        selector = CSSSelector('.reviews-num')
+        match = self.tree.xpath(selector.path)
+        total_reviews = int(match[0].text.replace(',', ''))
+        return total_reviews

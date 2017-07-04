972642bfa218307baa1ede6aa8ef2ca8e8bd76b1
Apoorva Pandey <apoorvapandey365@gmail.com>
2017-07-04 14:46:51 +0530

    Updates version to 0.2.1

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 import os
 from setuptools import find_packages, setup
 
-
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='playstats',
-    version='0.2',
+    version='0.2.1',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',
